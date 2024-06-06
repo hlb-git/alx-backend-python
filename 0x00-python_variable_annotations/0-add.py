03db79330e76264303543389974f3f2a22acac59
Love <mzeelovegeneral@gmail.com>
2024-06-06 20:48:30 +0100

    readme and statically typed function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Static funtion to add two numbers"""
+
+def add(a: float, b: float) -> float:
+    """Add two numbers"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+# Welcome to the 0x00-python_variable_annotations directory
