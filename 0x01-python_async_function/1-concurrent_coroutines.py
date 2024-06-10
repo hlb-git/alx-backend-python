ed14dfde169d873021f2b33f0a6107c9163c5aba
Love <mzeelovegeneral@gmail.com>
2024-06-10 23:01:13 +0100

    async funtions

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""The basics of async  """
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Wait for a random delay between n and max_delay"""
+    delays: List[float] = []
+    all_delays: List[float] = []
+    for i in range(n):
+        delays.append(wait_random(max_delay))
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays
